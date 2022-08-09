aaff060b6a14313b66502ab0d57169e7abf91d2b
herotesk <sashapoberezhnyjj0@gmail.com>
2022-08-09 21:27:12 +0300

    add database & add listall command

--- bin/scripterra-cli.js
+++ bin/scripterra-cli.js
@@ -5,9 +5,12 @@ const Compiler = require('../lib/compiler');
 
 const Configurator = require('../lib/configurator');
 const Console = require('../lib/console');
+const Database = require('../lib/database');
+const Scripts = require('../lib/database/entities/scripts');
 const FileSystem = require('../lib/filesystem');
 const Processor = require('../lib/processor');
 const Script = require('../lib/script');
+const ScriptConsole = require('../lib/script-console');
 
 const { defenitions } = require('./../lib/defenitions');
 
@@ -17,8 +20,11 @@ const consoleObj = new Console();
 const configurator = new Configurator(consoleObj);
 const filesystem = new FileSystem(consoleObj);
 const compiler = new Compiler(consoleObj, configurator, filesystem);
+const database = new Database([
+    Scripts
+]);
 
-const processor = new Processor(consoleObj, configurator, filesystem, compiler, args);
+const processor = new Processor(consoleObj, configurator, filesystem, compiler, database, args);
 
 process.addListener('uncaughtException', (err) => {
     console.log(err);
@@ -34,6 +40,8 @@ process.addListener('unhandledRejection', (err) => {
     consoleObj.consoleLogo();
     
     configurator.loadConfig();
+
+    await database.initDb();
 
     await processor.processCommand();
 
